b64117d7a4146f4a69fb9b3f560843e7e1744dc1
TechDev12 <58076224+TechDev12@users.noreply.github.com>
2025-01-09 20:50:39 +0200

    Update scan-and-block.bat

--- scan-and-block.bat
+++ scan-and-block.bat
@@ -1,5 +1,12 @@
-@ setlocal enableextensions 
+@echo off
+setlocal enableextensions
 set "target_path=%~1"
+set "allowed_subnet=192.168.1.0/24"  REM Change this to your subnet
+
 for /R "%target_path%" %%a in (*.exe) do (
+    rem Allow the program for the specified subnet
+    netsh advfirewall firewall add rule name="Allowed with Batchfile %%a" dir=out program="%%a" remoteip=%allowed_subnet% action=allow
+    
+    rem Block the program for all other traffic
     netsh advfirewall firewall add rule name="Blocked with Batchfile %%a" dir=out program="%%a" action=block
 )
